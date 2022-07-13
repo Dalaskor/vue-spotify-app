47f2a029ce553bd3680facf80172bb1016165b58
Dalaskor <dalas.prog@gmail.com>
2022-07-13 11:10:29 +0400

    Ready for work

---
+++ src/components/SpotifyApp.vue
@@ -0,0 +1,17 @@
+<template>
+  {{ test }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: 'testing',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
